5051622f7f173c4aa16028810c829aa93d07032e
Eric van Zanten <eric@bahai.us>
2013-10-18 21:18:59 -0500

    Finished scraping the aldermanic candidates that were in the original candidates.json. Tightened up the committees scraper

--- committee_scraper.py
+++ committee_scraper.py
@@ -31,58 +31,80 @@ class CommitteeScraper(DotNetScraper):
     def scrape_reports(self, url):
         start_page = self.lxmlize(url)
         committee_id = parse_qs(urlparse(url).query)['id'][0]
+        print url
         for page in self._grok_pages(start_page, committee_id):
-            reports = page.xpath("//tr[@class='%s']" % self.report_row)
-            for report in reports:
-                report_data = {}
-                raw_period = report.find("td[@headers='%s']/span" % self.report_period_cell).text
-                if raw_period:
-                    period = raw_period.split(' to ')
-                    if len(period) > 1:
-                        f_month, f_day, f_year = period[0].split('/')
-                        t_month, t_day, t_year = period[1].split('/')
-                        report_data['period_from'] = date(int(f_year), int(f_month), int(f_day))
-                        report_data['period_to'] = date(int(t_year), int(t_month), int(t_day))
+            if page is not None:
+                reports = page.xpath("//tr[@class='%s']" % self.report_row)
+                for report in reports:
+                    report_data = {}
+                    raw_period = report.find("td[@headers='%s']/span" % self.report_period_cell).text
+                    if raw_period:
+                        report_data['period_from'], report_data['period_to'] = self._parse_period(raw_period)
+                    detailed = report.find("td[@headers='%s']/a" % self.report_type_cell)
+                    if detailed is not None:
+                        report_data['type'] = detailed.text
+                        detail_url = detailed.attrib['href']
+                        report_data['detail_url'] = detail_url
+                        print detail_url
+                        report_id = parse_qs(urlparse(detail_url).query).get('id')
+                        if report_id:
+                            report_detail = self.lxmlize(detail_url)
+                            funds_start = report_detail.xpath("//span[@id='%s']" % self.detail_funds_start)
+                            if funds_start:
+                                report_data['funds_start'] = self._clean_float(funds_start[0].text)
+                            funds_end = report_detail.xpath("//span[@id='%s']" % self.detail_funds_end)
+                            if funds_end:
+                                report_data['funds_end'] = self._clean_float(funds_end[0].text)
+                            expenditures = report_detail.xpath("//span[@id='%s']" % self.detail_expend)
+                            if expenditures:
+                                report_data['expenditures'] = self._clean_float(expenditures[0].text)
+                            receipts = report_detail.xpath("//span[@id='%s']" % self.detail_receipts)
+                            if receipts:
+                                report_data['receipts'] = self._clean_float(receipts[0].text)
                     else:
-                        year = raw_period.split(' ')[0]
-                        try:
-                            report_data['period_from'] = date(int(year), 1, 1)
-                        except ValueError:
-                            pass
-                detailed = report.find("td[@headers='%s']/a" % self.report_type_cell)
-                if detailed is not None:
-                    report_data['type'] = detailed.text
-                    detail_url = detailed.attrib['href']
-                    report_data['detail_url'] = detail_url
-                    report_id = parse_qs(urlparse(detail_url).query).get('id')
-                    if report_id:
-                        report_detail = self.lxmlize(detail_url)
-                        funds_start = report_detail.xpath("//span[@id='%s']" % self.detail_funds_start)
-                        if funds_start:
-                            report_data['funds_start'] = float(funds_start[0].text.replace(',', '').replace('$', ''))
-                        funds_end = report_detail.xpath("//span[@id='%s']" % self.detail_funds_end)
-                        if funds_end:
-                            report_data['funds_end'] = float(funds_end[0].text.replace(',', '').replace('$', ''))
-                        expenditures = report_detail.xpath("//span[@id='%s']" % self.detail_expend)
-                        if expenditures:
-                            report_data['expenditures'] = float(expenditures[0].text.replace(',', '').replace('$', ''))
-                        receipts = report_detail.xpath("//span[@id='%s']" % self.detail_receipts)
-                        if receipts:
-                            report_data['receipts'] = float(receipts[0].text.replace(',', '').replace('$', ''))
-                else:
-                    detailed = report.find("td[@headers='%s']/span" % self.report_type_cell)
-                    report_data['type'] = detailed.text
-                yield report_data
+                        detailed = report.find("td[@headers='%s']/span" % self.report_type_cell)
+                        raw_period = report.find("td[@headers='%s']/span" % self.report_period_cell).text
+                        if raw_period:
+                            report_data['period_from'], report_data['period_to'] = self._parse_period(raw_period)
+                        report_data['type'] = detailed.text
+                    yield report_data
 
     def _grok_pages(self, start_page, committee_id):
         page_counter = start_page.xpath("//span[@id='%s']" % self.page_counter)[0].text
-        page_count = (int(page_counter.split(' ')[-1]) / 15) + 1
-        if page_count > 1:
-            for page_num in range(page_count):
-                url = self.url_pattern % (committee_id, page_num)
-                yield self.lxmlize(url)
+        if page_counter:
+            page_count = (int(page_counter.split(' ')[-1]) / 15) + 1
+            if page_count > 1:
+                for page_num in range(page_count):
+                    url = self.url_pattern % (committee_id, page_num)
+                    yield self.lxmlize(url)
+            else:
+                yield start_page
         else:
-            yield start_page
+            yield None
+
+    def _clean_float(self, num):
+       num = num.replace(',', '').replace('$', '') 
+       if num.find('(') is 0:
+           num = '-' + num.replace('(', '').replace(')', '')
+       return float(num)
+
+    def _parse_period(self, raw_period):
+        period = raw_period.split(' to ')
+        period_to = None
+        period_from = None
+        if len(period) > 1:
+            f_month, f_day, f_year = period[0].split('/')
+            t_month, t_day, t_year = period[1].split('/')
+            period_from = date(int(f_year), int(f_month), int(f_day))
+            period_to = date(int(t_year), int(t_month), int(t_day))
+        else:
+            year = raw_period.split(' ')[0]
+            try:
+                period_from = date(int(year), 1, 1)
+            except ValueError:
+                pass
+        return period_from, period_to
+        
 
 if __name__ == "__main__":
     committees = []
@@ -94,7 +116,8 @@ if __name__ == "__main__":
     for committee in committees:
         for report_data in scraper.scrape_reports(committee.url):
             report_data['committee'] = committee
-            report = Report(**report_data)
-            db.session.add(report)
-            db.session.commit()
-            print 'Saved %s' % report
+            report = Report.query.filter_by(**report_data).first()
+            if not report:
+                report = Report(**report_data)
+                db.session.add(report)
+                db.session.commit()
