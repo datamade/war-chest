a47beaa68fbdd54eb2c3e289a0f74196a1ff9ba0
Eric van Zanten <eric.vanzanten@gmail.com>
2014-03-07 19:54:23 -0600

    Added a couple more exceptions

--- committee_exceptions.py
+++ committee_exceptions.py
@@ -1,22 +1,45 @@
-from app import db, Candidate, Committee
+from app import db, Candidate, Committee, Report
 
 def add_pdf_reports():
     the_7th = Report.query.get(522550)
-    the_7th.funds_end
+    the_7th.funds_start = 40.05
+    the_7th.receipts = 0.0
+    the_7th.expenditures = 29.97
+    the_7th.invest_total = 0.0
+    the_7th.funds_end = 10.08
+    db.session.add(the_7th)
+    db.session.commit()
 
 def add_exceptions():
+    # Add proco Joe committee
     joe = Candidate.query.get(24885)
     first = Committee.query.get(24042)
     joe.committees.append(first)
     db.session.add(joe)
+    
+    # Add Emma Mitts committee
     emma = Candidate.query.get(15421)
     new_37 = Committee.query.get(18118)
     emma.committees.append(new_37)
     db.session.add(emma)
+
+    # Add Roberto Maldonado committee
     robert = Candidate.query.get(23923)
     the_26 = Committee.query.get(15492)
     robert.committees.append(the_26)
     db.session.add(robert)
+
+    # Add Bob Fioretti committee
+    bob = Candidate.query.get(25571)
+    second = Committee.query.get(23191)
+    bob.committees.append(second)
+    db.session.add(bob)
+
+    # Add Latasha Thomas 17th ward org
+    latasha = Candidate.query.get(15655)
+    seventeenth = Committee.query.get(16229)
+    latasha.committees.append(seventeenth)
+    db.session.add(latasha)
     db.session.commit()
     return None
 
@@ -55,5 +78,6 @@ def remove_exceptions():
     return None
 
 if __name__ == "__main__":
+    add_pdf_reports()
     add_exceptions()
     remove_exceptions()
--- dump_committees.py
+++ dump_committees.py
@@ -1,6 +1,7 @@
-from app import Candidate
+from app import Candidate, Report
 import csv
 from collections import OrderedDict
+from sqlalchemy import or_
 
 if __name__ == "__main__":
     dump = []
@@ -12,6 +13,18 @@ if __name__ == "__main__":
                 c['committee'] = comm.name
                 c['url'] = comm.url
                 c['position'] = 'In Support of Candidate'
+                rep = Report.query.filter(Report.committee_id == comm.id)\
+                    .filter(or_(Report.type.like('Quarterly%'), 
+                            Report.type.like('D-2 Semiannual Report%')))\
+                    .order_by(Report.date_filed.desc()).first()
+                if rep:
+                    c['current_funds'] = rep.funds_end
+                    c['invest_total'] = rep.invest_total
+                    c['total_assets'] = rep.funds_end + rep.invest_total
+                else:
+                    c['current_funds'] = None
+                    c['invest_total'] = None
+                    c['total_assets'] = None
                 dump.append(c)
         for comm in cand.committee_positions:
             if comm.committee.status == 'Active':
@@ -20,6 +33,18 @@ if __name__ == "__main__":
                 c['committee'] = comm.committee.name
                 c['url'] = comm.committee.url
                 c['position'] = 'Candidate is %s' % comm.title
+                rep = Report.query.filter(Report.committee_id == comm.committee.id)\
+                    .filter(or_(Report.type.like('Quarterly%'), 
+                            Report.type.like('D-2 Semiannual Report%')))\
+                    .order_by(Report.date_filed.desc()).first()
+                if rep:
+                    c['current_funds'] = rep.funds_end
+                    c['invest_total'] = rep.invest_total
+                    c['total_assets'] = rep.funds_end + rep.invest_total
+                else:
+                    c['current_funds'] = None
+                    c['invest_total'] = None
+                    c['total_assets'] = None
                 dump.append(c)
     out = open('dump.csv', 'wb')
     outp = csv.DictWriter(out, fieldnames=dump[0].keys())
