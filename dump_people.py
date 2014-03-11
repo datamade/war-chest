f7f2fd8f853a56625f38f4725791e6758187cb4d
Eric van Zanten <eric.vanzanten@gmail.com>
2014-03-10 19:39:19 -0500

    Regenerated person mapper

--- dump_people.py
+++ dump_people.py
@@ -1,30 +1,25 @@
 from app import Person, db
 import csv
 from operator import itemgetter
+from collections import OrderedDict
 
 if __name__ == "__main__":
     dump = []
     for person in Person.query.all():
-        d = {
-            'person_name': person.name,
-            'person_id': person.id,
-            'candidate_name': '',
-            'candidate_id': '',
-            'officer_name': '',
-            'officer_id': '',
-        }
         for cand in person.candidacies.all():
+            d = OrderedDict()
+            d['person_name'] = person.name,
+            d['person_id'] = person.id,
             d['officer_name'] = ''
             d['officer_id'] = ''
             d['candidate_name'] = cand.name
             d['candidate_id'] = cand.id
             if d not in dump:
                 dump.append(d)
-        d = {
-            'person_name': person.name,
-            'person_id': person.id
-        }
         for officer in person.committee_positions.all():
+            d = OrderedDict()
+            d['person_name'] = person.name,
+            d['person_id'] = person.id,
             d['candidate_name'] = ''
             d['candidate_id'] = ''
             d['officer_name'] = officer.name
